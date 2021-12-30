fa5b6b9da3d648fc27a30f739a2cc5c3a51eb07e
Nicola <lanzoni.nicola@gmail.com>
2021-12-30 16:40:20 -0300

    Update Core Module components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import {
+  TuiButtonModule,
+  TuiDataListModule,
+  TuiDropdownModule,
+  TuiHostedDropdownModule,
+  TuiSvgModule,
+} from '@taiga-ui/core';
+import { TuiActiveZoneModule, TuiLetModule } from '@taiga-ui/cdk';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from './header/header.component';
 import { StickyDirective } from './header/sticky.directive';
-import { TuiSvgModule } from '@taiga-ui/core';
+import { DatalistRowDropdownComponent } from './datalist-row-dropdown/datalist-row-dropdown.component';
+import { DataListRowActionProvider } from './services/dataListRowActionProvider';
+import { UserListRowActionsService } from '../users/services/user-list-row-actions.service';
 
 @NgModule({
   declarations: [
@@ -13,13 +25,26 @@ import { TuiSvgModule } from '@taiga-ui/core';
     StickyDirective,
     SidebarComponent,
     NavigationComponent,
+    DatalistRowDropdownComponent,
   ],
-  imports: [CommonModule, TuiSvgModule],
+  imports: [
+    CommonModule,
+    TuiSvgModule,
+    TuiDataListModule,
+    TuiHostedDropdownModule,
+    TuiDropdownModule,
+    TuiActiveZoneModule,
+    TuiButtonModule,
+    TuiLetModule,
+    RouterModule,
+  ],
   exports: [
     HeaderComponent,
     SidebarComponent,
     SidebarComponent,
     NavigationComponent,
+    DatalistRowDropdownComponent,
+    TuiSvgModule,
   ],
 })
 export class CoreModule {}
--- src/app/core/interfaces/navItems.ts
+++ src/app/core/interfaces/navItems.ts
@@ -2,6 +2,7 @@ export interface NavItem {
   title: string;
   route: string;
   icon?: string;
+  tooltip?: string;
 }
 
 export type NavItems = NavItem[] | null;
