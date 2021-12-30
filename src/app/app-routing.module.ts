6905d704ef61733a61ed9163e68090b0d054f9de
Nicola <lanzoni.nicola@gmail.com>
2021-12-30 16:41:00 -0300

    Created the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./users/users.module').then((m) => {
+        return m.UsersModule;
+      }),
+  },
+  // {
+  //   path: 'candidates',
+  //   loadChildren: () =>
+  //     import('./users/users.module').then((m) => {
+  //       return m.UsersModule;
+  //     }),
+  // },
+  // {
+  //   path: 'job-ad',
+  //   loadChildren: () =>
+  //     import('./users/users.module').then((m) => {
+  //       return m.UsersModule;
+  //     }),
+  // },
+  // { path: '**', component: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
